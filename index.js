97ed5e085f507acc0e85bda7a1d70bc50e322a08
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-12-03 14:02:24 -0800

    inputs

--- index.js
+++ index.js
@@ -6,3 +6,41 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
+inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'position',
+        message: 'What position is this employee?',
+        choices: [
+            'Manager',
+            'Intern',
+            'Engineer'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email address of this employee?',
+        
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the id of this employee?',
+    }
+]).then(({ position }) => {
+    switch (position) {
+        case 'Manager':
+        
+
+        case 'Intern':
+
+
+        case 'Engineer':
+
+        
+        default:
+
+    }
+
+})
