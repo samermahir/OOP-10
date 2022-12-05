743acde9d732a8e9ba3b3b110d116ff8e7197b45
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-12-05 13:50:42 -0800

    fixing cards

--- index.js
+++ index.js
@@ -122,11 +122,15 @@ function another() {
 
 const managerCard = function(manager) {
     return `
-    <div class="card">
+    <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">${manager.name}</h5>
-            <p class="card-text">${manager.email}</p>
-            <p class="card-text">${manager.officeNumber}</p>
+            <h5 class="card-header">${manager.name}</h5>
+            <div class="card-subtitle mb-2 text-muted">${manager.role}</div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: ${manager.email}</li>
+                 <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
             </div>
             </div>
             `;
@@ -134,11 +138,15 @@ const managerCard = function(manager) {
 
 function engineerCard(engineer) {
     return `
-    <div class="card">
+    <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">${engineer.name}</h5>
-            <p class="card-text">${engineer.email}</p>
-            <p class="card-text">${engineer.github}</p>
+            <h6 class="card-subtitle mb-2 text-muted">${engineer.role}</h6>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: ${engineer.email}</li>
+                 <li class="list-group-item">Github: ${engineer.github}</li>
+            </ul>
             </div>
             </div>
             `;
@@ -146,11 +154,15 @@ function engineerCard(engineer) {
 
 function internCard(intern) {
     return `
-    <div class="card">
+    <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">${intern.name}</h5>
-            <p class="card-text">${intern.email}</p>
-            <p class="card-text">${intern.officeNumber}</p>
+            <h6 class="card-subtitle mb-2 text-muted">${intern.role}</h6>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: ${intern.email}</li>
+                 <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
             </div>
             </div>
             `;
